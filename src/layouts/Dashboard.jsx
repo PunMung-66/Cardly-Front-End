e8de5cb723440377bde05a9e4e173c0a483d53c2
Punnawat <punnawat.mungkalarungsi@gmail.com>
2025-04-10 10:59:39 +0700

    Fix import case for Sidebar component in Dashboard layout

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../components/sidebar.jsx'
+import Sidebar from '../components/Sidebar'
 
 export default function Dashboard() {
     return (
