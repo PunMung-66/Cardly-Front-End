4674b789fa1431c0719a8d4156d470985ade5614
Asirum179 <Pawat6500@gmail.com>
2025-04-24 22:04:29 +0700

    Adjust login-sign up pages animation

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -49,7 +49,7 @@ export default function Login() {
         <>
             <AnimatePresence mode="wait">
                 <motion.div 
-                    initial= {{ opacity: 0, y: 20 }}
+                    initial= {{ opacity: 0, y: 0 }}
                     animate= {{ opacity: 1, y: 0 }}
                     exit= {{ opacity: 0, y: -20 }}
                     transition={{ duration: 0.4}}
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -56,7 +56,7 @@ export default function Signup() {
         <>
             <AnimatePresence mode="wait">
                 <motion.div
-                    initial= {{ opacity: 0, y: 20 }}
+                    initial= {{ opacity: 0, y: 0 }}
                     animate= {{ opacity: 1, y: 0 }}
                     exit= {{ opacity: 0, y: -20 }}
                     transition={{ duration: 0.4}}
